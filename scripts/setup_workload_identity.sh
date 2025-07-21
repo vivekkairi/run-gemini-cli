ef770937477237422193f5fbf757f6a5ad7c9871
Jerop Kipruto <jerop@google.com>
2025-07-21 15:34:23 -0400

    chore: add a license (#23)
    this is required by the chore tool

--- scripts/setup_workload_identity.sh
+++ scripts/setup_workload_identity.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Universal Direct Workload Identity Federation Setup Script for GitHub Actions
 # This script sets up Google Cloud Direct Workload Identity Federation for any GitHub repository
 # to work with the google-github-actions/auth action.
